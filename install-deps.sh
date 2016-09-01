e6588c2c66c5f98a96ccbf20c979a7774a771995
Fernando Jorge Mota <f.j.mota13@gmail.com>
2016-09-01 12:23:36 -0300

    Remove unneeded install-deps.sh file

--- install-deps.sh
+++
@@ -1,4 +0,0 @@
-apt-get update && apt-get install -y wget tar gzip bin86 tcsh lib32stdc++6 libc6-i386 libc6-dev-i386 lib32stdc++6 libc6-i386 libc6-dev-i386 lib32z1 lib32ncurses5 lib32bz2-1.0 qemu-kvm make build-essential bc
-wget https://github.com/Yelp/dumb-init/releases/download/v1.1.3/dumb-init_1.1.3_amd64.deb && dpkg -i dumb-init_*.deb
-wget --no-check-certificate -O ia32.tar.gz https://epos.lisha.ufsc.br/dl87 && tar -zxvf ia32.tar.gz && mkdir  -p /usr/local/ia32/ && mv gcc-4.4.4 /usr/local/ia32/
-
